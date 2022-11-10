f86fef10029693f33e4274456cb3c1b8093027bb
lvhungdeveloper <quyet.nguyen@ukparkingcontrol.com>
2022-11-10 18:25:57 +0700

    init project

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <Grid>
+            <Typography>asd</Typography>
+        </Grid>
+    );
+}
